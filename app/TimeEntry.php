d3c0ba0accb26a040bd422d20f1df6f1ca422b6b
Manu <bionikspoon@gmail.com>
2015-05-05 12:59:01 -0500

    setup model relations

--- app/TimeEntry.php
+++ app/TimeEntry.php
@@ -5,8 +5,22 @@ namespace app;
 
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TimeEntry extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'time_entry';
+
+    protected $dates = ['deleted_at'];
+
+    protected $fillable = ['user_id', 'start_time', 'end_time', 'comment'];
+
+    protected $hidden = ['user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
--- app/User.php
+++ app/User.php
@@ -9,13 +9,32 @@ use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 
 class User extends Model implements AuthenticatableContract, CanResetPasswordContract
 {
 
-    use Authenticatable, CanResetPassword;
+    use Authenticatable, CanResetPassword, SoftDeletes;
 
+    protected $dates = ['deleted_at'];
 
+    protected $visible = [
+        'id',
+        'first_name',
+        'last_name',
+        'email',
+        'remember_token'
+    ];
 
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+    ];
+
+    public function timeEntries()
+    {
+        $this->hasMany('App\TimeEntry');
+    }
 }
