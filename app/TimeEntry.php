b2936cf91db55ec0abee2c15b455c9b479c50c8e
Manu <bionikspoon@gmail.com>
2015-05-05 00:53:49 -0500

    Eloquent Models, model migrations

---
+++ app/TimeEntry.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace app;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class TimeEntry extends Model
+{
+    protected $table = 'time_entries';
+}
---
+++ app/User.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace app;
+
+
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+
+
+class User extends Model implements AuthenticatableContract, CanResetPasswordContract
+{
+
+    use Authenticatable, CanResetPassword;
+
+
+
+}
