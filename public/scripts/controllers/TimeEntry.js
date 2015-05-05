f6a6c1509da2f10afdd5a1edec66aa0e7b9b2b36
Manu <bionikspoon@gmail.com>
2015-05-04 20:21:03 -0500

    setup time form

--- public/scripts/controllers/TimeEntry.js
+++ public/scripts/controllers/TimeEntry.js
@@ -2,8 +2,16 @@
   "use strict";
   angular.module('timeTracker').controller('TimeEntry', TimeEntry);
 
-  function TimeEntry($time) {
-    var vm = $this;
+  function TimeEntry(time) {
+    var vm = this;
     vm.timeentries = [];
+
+    time.getTime().then(
+      function (results) {
+        vm.timeentries = results;
+        console.log(vm.timeentries);
+      }, function (error) {
+        console.log(error);
+      })
   }
 })();
--- public/scripts/services/time.js
+++ public/scripts/services/time.js
@@ -4,6 +4,18 @@
 
   function time($resource) {
     var Time = $resource('data/time.json');
-    return {}
+
+    function getTime() {
+      return Time.query().$promise.then(
+        function (results) {
+          return results;
+        }, function (error) {
+          console.log(error);
+        })
+    }
+
+    return {
+      getTime: getTime
+    }
   }
 })();
